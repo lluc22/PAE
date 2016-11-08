84fe61268d78dd54d766b08dec7c1b8cfcf58d44
Daniel Farre <daniel.farre@est.fib.upc.edu>
2016-11-08 12:55:15 +0100

    Change bello.js ip

--- Api1/testing/hello.js
+++ Api1/testing/hello.js
@@ -5,5 +5,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(8080, '45.55.176.54');
-console.log('Server running at http://45.55.176.54:8080/');
+}).listen(8080, '84.88.81.126');
+console.log('Server running at http://84.88.81.126:8080/');
