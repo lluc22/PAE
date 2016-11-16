118f3302e8213ccee6745cdfb86519b3b0b59c23
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2016-11-16 16:27:42 +0100

    Working on server, added related tickets and users to tickets info(answeres,questions)

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -26,7 +26,8 @@ app.config(['$locationProvider', '$routeProvider', function($locationProvider, $
 
 app.service('getTicket', function ($http) {
     this.getTicketContent = function (id) {
-        return $http.get('http://localhost:8080/api/ticket/'+id)
+        //return $http.get('http://localhost:8080/api/ticket/'+id)
+        return $http.get('http://84.88.81.126:8080/api/ticket/'+id)
             .then(function successCallback(response) {
                 //var values = response['data']['msg'][0];
                 //alert(response);
@@ -40,12 +41,22 @@ app.service('getTicket', function ($http) {
 
             });
     };
+    this.getTicketRelateds = function (id) {
+        return $http.get('http://84.88.81.126:8080/api/ticket/'+id+'/related')
+            .then(function successCallback(response) {
+                console.log(response['data']['msg']['data'][0]);
+                return response['data']['msg']['data'][0];
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+    };
     return this;
 });
 
 app.service('getUser', function ($http) {
-    this.getUserInfo = function (id, vec) {
-        return $http.get('http://localhost:8080/api/user/'+id)
+    this.getUserInfo = function (id) {
+        //return $http.get('http://localhost:8080/api/user/'+id)
+        return $http.get('http://84.88.81.126:8080/api/user/'+id)
             .then(function successCallback(response) {
                 //var values = response['data']['msg'][0];
                 //alert(response);
@@ -70,59 +81,64 @@ app.controller('ticketController', function ($scope, $http, getTicket, getUser) 
     //console.log(window.location.href);
     var string = window.location.href; // con esto cojo la url, solo me interesa quedarme el ultimo numero de esta
     var id = string.split('/');
-    var a =  [];
+    var answer_users =  [];
+    var question_user;
     id = id[id.length - 1];
-    //var namesId = [];
-    var vec = function () {
-        //console.log("length-->" +a.length);
-        if(a.length == answers){
+    $scope.relateds = [];
 
-            for(var i =0; i<a.length;++i)
-                document.getElementsByClassName('user')[i].innerHTML = a[i];
+    var getAnswersUser = function () {
+        //console.log("length-->" +a.length);
+        if(answer_users.length == answersCount){
+
+            for(var i =0; i<answer_users.length;++i)
+                document.getElementsByClassName('user')[i].innerHTML =
+                    "<p align='right'> <i> response by: " + answer_users[i] +
+            "</i></p>";;
         }
     };
-    var answers;
+
+    var getQuestionUser = function () {
+        document.getElementById("question-user").innerHTML = "<p align='right' style='margin-right:2em;'> <i> Question made by: " + question_user +
+            "</i></p>";
+    };
+
+    var answersCount;
     getTicket.getTicketContent(id).then(function (resp) {
-        answers = resp.answers.length;
+        answersCount = resp.answers.length;
         $scope.title = resp.title;
-        //$scope.body = resp.body;
-        document.getElementById('question').innerHTML = resp.body;
+        document.getElementById('question').innerHTML = "<table><tbody><tr><td>" + resp.body + "</td></tr>" +
+            "<tr><td id='question-user'></td></tr>";
+        getUser.getUserInfo(resp.ownerUserId).then(function (resp) {
+                question_user = resp.displayName;
+                getQuestionUser();
+        });
         document.getElementById('answers-header').innerHTML = "<h4>" + resp.answers.length + " Answers </h4>";
         if (resp.answers.length > 0) {
             for (var i = 0; i < resp.answers.length; ++i) {
                 document.getElementById('answers-content').innerHTML +=
-                    "<table><tbody><tr><td class='user'></td><td class='answercell'>" +
+                    "<table><tbody><tr><td class='answercell'></td></tr><tr><td class='user'>" +
                     "</td></tr></tbody></table>";
                 document.getElementsByClassName('answercell')[i].innerHTML = resp.answers[i].body;
                 //document.getElementsByClassName('user')[i].innerHTML = resp.answers[i].ownerId;
                 //namesId.push(resp.answers[i].owenerId);
 
-                 getUser.getUserInfo(resp.answers[i].ownerId,vec).then(function (resp2) {
-                     a.push(resp2.displayName);
-                     vec();
-                    console.log(resp2.displayName);
-                 //console.log(names);
-
-
+                 getUser.getUserInfo(resp.answers[i].ownerId).then(function (resp2) {
+                     answer_users.push(resp2.displayName);
+                     getAnswersUser();
                  });
-
-
-
-
-                /*
-                 document.getElementById('answers-content').innerHTML +=
-                 "<table><tbody><tr><td class='user'>" + resp.answers[i].ownerId +"</td><td class='answercell'>" + resp.answers[i].body +
-                 "</td></tr></tbody></table>";
-
-                 */
-
             }
 
         }
     });
-
     /*
-    }*/
+    getTicket.getTicketRelateds(id).then(function (resp) {
+       console.log('This is the resp');
+    });
+    */
+    for (var i = 0; i<20; ++i) {
+        $scope.relateds.push("Ticket"+(i+1));
+    }
+
 
 });
 
@@ -319,50 +335,14 @@ function createCORSRequest(method, url, callback, limit, skip) {
         }
     };
     xhr.send();
-
-
-    /*
-    if ("withCredentials" in xhr) {
-        // XHR for Chrome/Firefox/Opera/Safari.
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        // XDomainRequest for IE.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        // CORS not supported.
-        xhr = null;
-    }
-
-    return xhr;
-    */
 }
 
 // Make the actual CORS request.
 function makeCorsRequest(callback, limit, skip) {
     // This is a sample server that supports CORS.
-    //var url = 'http://84.88.81.126:8080/api/tickets';
+    var url = 'http://84.88.81.126:8080/api/tickets';
+    //var url = 'http://localhost:8080/api/tickets';
 
-    var url = 'http://localhost:8080/api/tickets';
     var xhr = createCORSRequest('GET', url, callback, limit, skip);
     //console.log('makeCorsRequest---> ' + xhr);
-    /*
-    if (!xhr) {
-       // alert('CORS not supported');
-        return;
-    }
-
-    var text;
-    // Response handlers.
-    xhr.onload = function() {
-        text = xhr.responseText;
-        //alert('Response from CORS request to ' + url + ': ' + title);
-    };
-
-    xhr.onerror = function() {
-        //alert('Woops, there was an error making the request.');
-    };
-
-    xhr.send();
-    */
 }
