80875a45ccbfa1a3c419c0fb3da10d658773f1fe
Daniel Farre <daniel.farre@est.fib.upc.edu>
2016-11-24 16:27:21 +0100

    puts conflicts

--- Api1/app/services/doc2vec/doc2vec.js
+++ Api1/app/services/doc2vec/doc2vec.js
@@ -1,32 +1,32 @@
 var PythonShell = require('python-shell');
-var Parser = require('../parse.js')
+var Parser = require('../parse.js');
 var pyshell = new PythonShell('./app/services/doc2vec/doc2vec.py',{ mode: 'json', pythonPath:'/usr/local/bin/python3'});
 var fs = require('fs');
 // sends a message to the Python script via stdin
-var chunkSize = 10000
-var chunks = 24
-var c = 0
-var model_path = ""
-var size = 239932
-var busy = false
-var firstTopN = true
+var chunkSize = 10000;
+var chunks = 24;
+var c = 0;
+var model_path = "";
+var size = 239932;
+var busy = false;
+var firstTopN = true;
 //pyshell.send({command:"build_vocab"})
 pyshell.on('message', function (message) {
   //console.log(message)
-  command = message['command']
+  command = message['command'];
   switch (command) {
     case "build_vocab":
-      var posts = []
+      var posts = [];
       if(c < chunks){
         //We simulate DB reading from chunk files
         fs.readFile('posts/'+c+'.txt', 'utf8', function (err,data) {
           if (err) {
             return console.log(err)
           }
-          console.log(c)
-          posts = JSON.parse(data)
-          c++
-          console.log("sending")
+          console.log(c);
+          posts = JSON.parse(data);
+          c++;
+          console.log("sending");
           pyshell.send({command:"build_vocab",finished:false,posts:posts})
         });
       }
@@ -36,17 +36,17 @@ pyshell.on('message', function (message) {
       break;
 
     case "train":
-      var finished = message['finished']
+      var finished = message['finished'];
       if(!finished){
-        var posts = []
-        var chunk = message['chunk']
-        console.log(chunk)
+        var posts = [];
+        var chunk = message['chunk'];
+        console.log(chunk);
         fs.readFile('posts/'+chunk+'.txt', 'utf8', function (err,data) {
           if (err) {
             return console.log(err)
           }
-          posts = JSON.parse(data)
-          c++
+          posts = JSON.parse(data);
+          c++;
           pyshell.send({command:"train",posts:posts})
         });
       }
@@ -63,13 +63,13 @@ pyshell.on('message', function (message) {
 module.exports = {
   train: function(givePathCallback){
     if(!busy){
-      busy = true
-      pyshell.send({command:"build_vocab"})
+      busy = true;
+      pyshell.send({command:"build_vocab"});
       pyshell.on('message', function(message){
         if(message['command'] == 'train'){
             if(message['finished']){
-            model_path = message['model_path']
-            givePathCallback(model_path)
+            model_path = message['model_path'];
+            givePathCallback(model_path);
             busy = false
             }
         }
@@ -78,13 +78,13 @@ module.exports = {
   },
   load_model: function(path,okCallBack){
     if(!busy){
-      busy = true
-      pyshell.send({command:"load_model",path:path})
+      busy = true;
+      pyshell.send({command:"load_model",path:path});
       pyshell.on('message',function(message){
           if(message['command'] == 'load_model'){
-              if(!message['ok']) console.log(message['err'])
-              busy = false
-              console.log("model loaded")
+              if(!message['ok']) console.log(message['err']);
+              busy = false;
+              console.log("model loaded");
               okCallBack()
             }
       });
@@ -92,14 +92,14 @@ module.exports = {
   },
   vectors: function(topn,fillVectorCallback){
     if(!busy){
-      busy = true
+      busy = true;
       pyshell.on('message',function(message){
-        command = message['command']
+        command = message['command'];
         if(command == "get_vectors"){
-          fillVectorCallback({id:message['id'],vector:message['vector'],topn:message['topn']})
+          fillVectorCallback({id:message['id'],vector:message['vector'],topn:message['topn']});
           if(message['finish']){
-             busy = false
-             console.log("I finnished")
+             busy = false;
+             console.log("I finnished");
            }
         }
       });
@@ -109,18 +109,18 @@ module.exports = {
 
   topn_similar: function(id,topn,fillIdCallback){
     if(!busy){
-      busy = true
+      busy = true;
       if(firstTopN){
         pyshell.on('message',function(message){
-          command = message['command']
+          command = message['command'];
           if(command == "topn"){
-            busy = false
-            fillIdCallback(message['ids'])
+            busy = false;
+            fillIdCallback(message['ids']);
           }
         });
-        firstTopN = false
+        firstTopN = false;
       }
-      pyshell.send({command:"topn",n:topn,id:id})
+      pyshell.send({command:"topn",n:topn,id:id});
     }
   }
 
