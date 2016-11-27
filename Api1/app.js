c40a8f379d7b7959b92dbafffc28f24ed12cbea3
Daniel Farre <daniel.farre@est.fib.upc.edu>
2016-11-27 19:43:50 +0100

    new endpoints

--- Api1/app.js
+++ Api1/app.js
@@ -54,6 +54,7 @@ require('./config/passport')(passport);
 // bundle our routes
 var apiRoutes = express.Router();
 
+/*
 // create a new user account (POST http://localhost:8080/api/signup)
 apiRoutes.post('/signup', function(req, res) {
   if (!req.body.name || !req.body.password ) {
@@ -72,6 +73,7 @@ apiRoutes.post('/signup', function(req, res) {
     });
   }
 });
+*/
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -83,6 +85,7 @@ app.use(function(req, res, next) {
 // connect the api routes under /api/*
 app.use('/api', apiRoutes);
 
+/*
 apiRoutes.post('/authenticate', function(req, res) {
   User.findOne({
     name: req.body.name
@@ -126,7 +129,7 @@ apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), fu
   } else {
     return res.status(403).send({success: false, msg: 'No token provided.'});
   }
-});
+});*/
 
 
 
@@ -142,7 +145,6 @@ getToken = function (headers) {
     return null;
   }
 };
-
 
 var myCallback = function(data) {
   var newPost = new Post({
@@ -221,54 +223,96 @@ apiRoutes.get('/fillingusers', function(req, res) {
 
 //tiquets
 apiRoutes.get('/tickets', function(req, res) {
-        Post.find({
-          acceptedAnswerId: { $exists: false}
-        }, {id: 1, title: 1}, function(err, posts) {
-          if (err) throw err;
-          res.json({success: 200, msg: {"data": posts}});
-        }).limit(parseInt(req.headers.limit)).skip(parseInt(req.headers.skip));
+    Post.find({
+        acceptedAnswerId: { $exists: false}
+    }, {id: 1, title: 1, _id: 0}, function(err, posts) {
+        if (err) throw err;
+        res.json({success: 200, msg: {"data": posts}});
+    }).limit(parseInt(req.headers.limit)).skip(parseInt(req.headers.skip));
+});
+
+apiRoutes.get('/ticketsclosed', function(req, res) {
+    Post.find({
+        acceptedAnswerId: { $exists: true}
+    }, {id: 1, title: 1, _id: 0}, function(err, posts) {
+        if (err) throw err;
+        res.json({success: 200, msg: {"data": posts}});
+    }).limit(parseInt(req.headers.limit)).skip(parseInt(req.headers.skip));
 });
 
 apiRoutes.get('/ticket/:id', function(req, res) {
   Post.find({
     id: req.params.id
-  }, function(err, posts) {
+  }, {_id: 0, __v: 0, "answers._id": 0, "tags._id": 0}, function(err, posts) {
     if (err) throw err;
     res.json({success: 200, msg: {"data": posts}});
   });
 });
 
-/*var doc2vecCallback = function(data) {
-  var newPost = new Post({
-    id: data['id'],
-    acceptedAnswerId: data['acceptedAnswerId'],
-    creationDate: data['creationDate'],
-    body: data['body'],
-    ownerUserId: data['ownerUserId'],
-    closedDate: data['closedDate'],
-    title: data['title'],
-    tags: data['tags']
-  });
-
-};*/
-
 apiRoutes.get('/ticket/:id/related', function(req, res) {
   var callbackCounter = 0;
   var doc2vecCallback = function(data) {
-    //res.json({success: 200, msg: {"data": data }});
-    //res.json({success: 200, msg: {"data": data }});
     callbackCounter++;
     if (callbackCounter == 1) {
-      res.json({success: 200, msg: {"data": data }});
+        var json = [];
+        data.forEach(function(postId) {
+            json.push(postId);
+        });
+        Post.find({ id: {$in: data }},{id: 1, title: 1, _id: 0}, function(err, post) {
+            if(post.length > 0) {
+                res.json({success: 200, msg: {"data": post }});
+                res.end();
+            }
+
+        });
+      if (data.length < 1) res.json({success: 200, msg: {"data": [] }});
+        //res.json({success: 200, msg: {"data": [] }});
     }
     else {
-      res.end();
+        res.end();
     }
-    //console.log(data);
   };
   var doc2vec = require('./app/services/doc2vec/doc2vec');
   doc2vec.topn_similar(req.params.id, 20, doc2vecCallback);
 });
+
+apiRoutes.get('/ticket/:id/related/users', function(req, res) {
+    var callbackCounter = 0;
+    var doc2vecCallback = function(data) {
+        callbackCounter++;
+        if (callbackCounter == 1) {
+            var json = [];
+            data.forEach(function(postId) {
+                json.push(postId);
+            });
+            Post.find({ id: {$in: data }, acceptedAnswerId: { $exists: true}, "bestAnswer.ownerId": { $exists: true}},{"bestAnswer.ownerId": 1, _id: 0}, function(err, post) {
+                if(post.length > 0) {
+                    var json = [];
+                    post.forEach(function (pot) {
+                        json.push(pot["bestAnswer"]["ownerId"]);
+                    });
+                    UserPost.find({id: {$in: json }}, {_id: 0, __v: 0}, function(err, users){
+                        res.json({success: 200, msg: {"data": users}});
+                        res.end();
+                    });
+                }
+            });
+            if (data.length < 1) res.json({success: 200, msg: {"data": [] }});
+        }
+        else {
+            res.end();
+        }
+    };
+    var doc2vec = require('./app/services/doc2vec/doc2vec');
+    try {
+        doc2vec.topn_similar(req.params.id, 20, doc2vecCallback);
+    }
+    catch (e) {
+
+    }
+
+});
+
 
 //users
 apiRoutes.get('/user/:id', function(req, res) {
@@ -279,6 +323,10 @@ apiRoutes.get('/user/:id', function(req, res) {
     res.json({success: 200, msg: {"data": user}});
   });
 });
+
+
+
+
 
 
 var actualitzaDB = function (DB) {
